b22e8080df3a52a679228655ae526c019e52db18
Bijoy99roy <bijoy2014amp@gmail.com>
2021-11-11 23:19:55 +0530

    main completed

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+from segmentation_mask.segmentation import Segmentation
+from utils.utils import Utils
 from flask import Flask, Response, render_template
 import cv2
+import os
+import random
 
 app = Flask(__name__)
 camera = cv2.VideoCapture(0)
+segmentation = Segmentation()
+utils = Utils()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -15,12 +21,31 @@ def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-def gen_frames():
+@app.route('/video_feed/<mode>', methods=['GET'])
+def video_feed_modified(mode):
+    return Response(gen_frames(int(mode)), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def gen_frames(mode=0, name=None):
+    print(mode)
+    randnum = random.randrange(0, 7)
     while True:
         success, frame = camera.read()  # read the camera frame
         if not success:
             break
         else:
+            if mode == 1:
+                mask, frame = segmentation.get_mask(frame)
+                frame = utils.remove_background(mask, frame)
+            elif mode == 2:
+                mask, frame = segmentation.get_mask(frame)
+                frame = utils.blur_background(mask, frame)
+            elif mode == 3:
+                lst = os.listdir('bg_images/')
+                path = 'bg_images/'+lst[randnum]
+                mask, frame = segmentation.get_mask(frame)
+                frame = utils.change_background(mask, frame, path)
+
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
