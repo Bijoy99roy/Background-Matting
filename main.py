8f544c6e280df8067afad515be093cbe5991b245
Bijoy99roy <bijoy2014amp@gmail.com>
2021-11-11 19:38:22 +0530

    video streaming added

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-from flask import Flask, request, render_template
-
+from flask import Flask, Response, render_template
+import cv2
 
 app = Flask(__name__)
+camera = cv2.VideoCapture(0)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
+
+@app.route('/video_feed', methods=['GET'])
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
