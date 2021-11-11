402cd2eb35c4760ed04aa2c1e33b755b20bb5d89
Bijoy99roy <bijoy2014amp@gmail.com>
2021-11-11 23:19:31 +0530

    refactored

--- segmentation_mask/segmentation.py
+++ segmentation_mask/segmentation.py
@@ -20,14 +20,14 @@ class Segmentation:
             with self.mp_selfie_segmentation.SelfieSegmentation(
                     model_selection=1) as selfie_segmentation:
 
-                frame = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGB)
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 # To improve performance, optionally mark the image as not writeable to
                 # pass by reference.
                 frame.flags.writeable = False
                 results = selfie_segmentation.process(frame)
                 frame.flags.writeable = True
                 frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-                condition = np.stack((results.segmentation_mask,), axis=-1) > 0
+                condition = np.stack((results.segmentation_mask,), axis=-1) > 0.3
                 self.mask = np.where(condition, self.fg_color, self.bg_color)
 
             return self.mask, frame
