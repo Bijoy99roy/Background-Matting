5cc1725c3db334c46b9f5084dbd72879d50ac7ea
Bijoy99roy <bijoy2014amp@gmail.com>
2021-11-11 20:15:39 +0530

    blurred image utils added

---
+++ utils/utils.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+class Utils:
+    def __init__(self):
+        self.output_frame = None
+
+    def blur_background(self, mask, frame):
+        """
+        Blur the background of the frame
+        parameters
+        ----------------------
+        mask: Extracted binary mask from the frame
+        frame: Input frame from the video stream
+        :return: blurred background image
+        """
+        try:
+            blurred_frame = cv2.GaussianBlur(frame, (55, 55), 0)
+            self.output_frame = np.where(mask == 0, blurred_frame, frame)
+
+            return self.output_frame
+        except Exception as e:
+            raise e
