edd4f6fd499f351c7f5d79132c91b034c4edb7d5
Bijoy99roy <bijoy2014amp@gmail.com>
2021-11-24 10:33:29 +0530

    removed database and switched to file based logging

--- application_logging/logger.py
+++ application_logging/logger.py
@@ -1,14 +1,13 @@
 # performing important imports
 from datetime import datetime
-from db_operations.database import DataBase
 
 
 class AppLogger:
-    def __init__(self):
-        self.database = DataBase()
-
-    def log(self, collection_name, message, level=''):
+    @staticmethod
+    def log(file_object, log_message, level=''):
         now = datetime.now()
         date = str(now.date())
         current_time = now.strftime('%H:%M:%S')
-        self.database.insert_data(collection_name, date, current_time, message, level)
+        file_object.write(
+            str(date) + '/' + str(current_time) + ': ' + str(level) + ' :: ' + '\t\t' + log_message + '\n'
+        )
